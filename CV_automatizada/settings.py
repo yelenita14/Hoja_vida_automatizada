4cfe1deda6b1557900800a8fc95ffb7ca6e19f8f
yelenita14 <anchundiakaren005@gmail.com>
2026-01-26 23:53:48 -0500

    proyecto

--- CV_automatizada/settings.py
+++ CV_automatizada/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (User uploads)
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
